e328c6a07dabf8488986ead8c1dbf853e8909fd4
GeanPeiraz <gean2014brasil@gmail.com>
2024-07-24 14:05:53 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+    public class Main {
+              public static void main(String[] args) {
+    ArrayList <integer> idades= new ArrayList <integer> ();
+    idade.add(22);
+    idade.add(33);
+    idade.add(44);
+    idade.add(55);
+    idade.remove(index:0);
+
