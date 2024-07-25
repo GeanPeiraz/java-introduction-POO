5435c9f67b1adb65898548c9710ebf5711e0e2e8
GeanPeiraz <gean2014brasil@gmail.com>
2024-07-25 11:34:10 -0300

    Intruction left - right

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
     public class Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
             System.out.println("First API-JAVA");
+        }
+        //static void myInstructionPrintln() {
+        /*System.out.println("This is a sample program"); */
     }
-    static void myInstructionPrintln() {
-        System.out.println("This is a sample program");
-    }
- }
 
