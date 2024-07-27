12dacdeb917b61457e331b9e8315820de9aa2ff6
GeanPeiraz <gean2014brasil@gmail.com>
2024-07-26 22:35:58 -0300

    Name

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
     public class Main {
     public static void main(String[]  args) {
-        var. login = "Name";
-
-
-            System.out.println("First API-JAVA");
+        var login = "Name";
+        login = "Gean";
+            System.out.println(login);
         }
         //static void myInstructionPrintln() {
         /*System.out.println("This is a sample program"); */
