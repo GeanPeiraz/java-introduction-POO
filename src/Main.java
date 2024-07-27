7f2063955c30272db806d674c3cfff8106e9c34c
GeanPeiraz <gean2014brasil@gmail.com>
2024-07-26 23:13:00 -0300

    new exemple

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
     public class Main {
 
     public static void main(String[]  args) {
-        var number1 = 10.03;
         var password = "123456";
 
         // var = tipo : Text (String)
@@ -12,10 +11,16 @@ import java.util.ArrayList;
 
         // Tipos de Dados
         // tipo nameReference = conteudoRef;
+
         String Login = "Names";
+        int number1 = 10;
+        double perc = 20.2;
+        boolean driver = true;
 
             System.out.println(password);
             System.out.println(number1);
+            System.out.println(perc);
+            System.out.println(driver);
         }
         //static void myInstructionPrintln() {
         /*System.out.println("This is a sample program"); */
