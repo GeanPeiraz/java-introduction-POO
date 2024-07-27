668b836f067ec0979a19fd923e7eccd50e660cda
GeanPeiraz <gean2014brasil@gmail.com>
2024-07-26 22:49:49 -0300

    Subindo uam Branch

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 import java.util.ArrayList;
     public class Main {
+
     public static void main(String[]  args) {
-        var login = "Name";
-        login = "Gean";
-            System.out.println(login);
+        var number1 = 10.03;
+        var password = "123456";
+
+        // var = tipo : Text (String)
+        // var = tipo : Number (Int)
+        // var = tipo : Number Frac. (Double)
+        // var = tipo : booleanas ( true / false )
+
+        // Tipos de Dados
+        // tipo nameReference = conteudoRef;
+        String Login = "Names";
+
+            System.out.println(password);
+            System.out.println(number1);
         }
         //static void myInstructionPrintln() {
         /*System.out.println("This is a sample program"); */
