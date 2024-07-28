87f2ea217c6132870ad12c47d949db8b6381fb7a
GeanPeiraz <gean2014brasil@gmail.com>
2024-07-27 21:43:25 -0300

    New array

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,13 @@ import java.util.ArrayList;
     public class Main {
 
     public static void main(String[]  args) {
-        var password = "123456";
+        int madruguinha;
 
-        // var = tipo : Text (String)
-        // var = tipo : Number (Int)
-        // var = tipo : Number Frac. (Double)
-        // var = tipo : booleanas ( true / false )
+        madruguinha = 55;
 
-        // Tipos de Dados
-        // tipo nameReference = conteudoRef;
+        System.out.println(madruguinha + 10);
 
-        String Login = "Names";
-        int number1 = 10;
-        double perc = 20.2;
-        boolean driver = true;
 
-            System.out.println(password);
-            System.out.println(number1);
-            System.out.println(perc);
-            System.out.println(driver);
         }
         //static void myInstructionPrintln() {
         /*System.out.println("This is a sample program"); */
